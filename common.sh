3ad3606612141248066a198db0878c3fd7fcfbad
chandana <allu.chandu05@gmail.com>
2026-02-04 19:20:15 -0600

    Actions

---
+++ common.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop1"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+B="\e[34m"
+N="\e[0m" #Normal
+
+echo "script started and executed at : $(date)" | tee -e $LOGS_FILE
+
+ mkdir -p $LOGS_FOLDER
+
+ check_root(){
+        if [ $USERID -ne 0 ]; then
+            echo -e "$R please run this script with root user access $N" | tee -a $LOGS_FILE
+            exit 1
+        fi  
+    }
+
+
+ VALIDATE(){
+            if [ $1 -ne 0 ]; then
+                echo -e "$(date "+%Y %M %D %H: %M: %S") | $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+                exit1
+            else
+                echo -e "$(date "+%Y %M %D %H: %M: %S") | $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+            fi
+        }
